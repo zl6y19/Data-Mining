9fb9290efce92c67838e8e5ca3b8a3e62e410339
Xinyan Lv <lvxinyan1992@gmail.com>
2016-03-08 16:40:58 +0000

    Remove the correct spelling function

--- data.py
+++ data.py
@@ -28,21 +28,23 @@ def getWordsTF(file_name):
         content_original = ''
         for i in soul.find_all('span', class_ = "ocr_cinfo"):
             #Lemmatization
-            content_original = content_original + ' ' + lemma(i.text)
+            content_original = content_original + ',' + lemma(i.text)
+
 
         # remove all non-alpha characters and convert to lowercase
         content = preprocessWords(content_original)
+
         # remove stopwords
         content = removestopword(content)
+
         # remove short words
         content = removeshort(content)
-        # correct the misspelling words
-        content = correctword(content)
-        # repeat remove stop word and short word
-
-
-
         print content
+
+
+
+
+        #print content
     return content
 
 
@@ -50,11 +52,16 @@ def getWordsTF(file_name):
 
 def preprocessWords(words):
 
+
     # Split words by all non-alpha characters
+    # 用了这个后出现了u''!!!!!!!!!!!!!!!!!!
     words = re.compile(r'[^A-Z^a-z]+').split(words)
 
     # Convert to lowercase
     return [word.lower() for word in words if word != '']
+
+def lowerword(words):
+    return [word.lower() for word in words if 1 == 1]
 
 def removestopword(words):
     # remove the stop words
@@ -66,23 +73,50 @@ def removeshort(words):
     # remove short word
     return [word for word in words if len(word) > 3]
 
-def correctword(words):
-    d = enchant.Dict("en_GB")
+#def correctword(words):
+    #d = enchant.Dict("en_GB")
     # check the word by the build-in Dic
     rightwords = ''
-    for word in words:
-        if d.check(word) == 'True':
-            rightwords = rightwords + ' ' + word
-        else:
-            suggestion = d.suggest(word)
-            if suggestion != '':
-                rightwords = rightwords + ' ' + suggestion[0]
-            else:
-                rightwords = rightwords + ' ' + word
-    return rightwords
+
+    #for word in words:
+        #print d.check(word)
+        #if d.check(word) == True:
+            #rightwords = rightwords + ' ' + word
+        #else:
+            #suggestion = d.suggest(word)
+            #print suggestion
+            #rightwords = rightwords + ' ' + suggestion[0]
+    #return rightwords
     # if true : add to string else check the suggestion
 
+def Tfidf(filelist) :
+    path = './segfile／'
+    corpus = []  #存取100份文档的分词结果
+    for ff in filelist :
+        fname = path + ff
+        f = open(fname,'r+')
+        content = f.read()
+        f.close()
+        corpus.append(content)
 
+    vectorizer = CountVectorizer()
+    transformer = TfidfTransformer()
+    tfidf = transformer.fit_transform(vectorizer.fit_transform(corpus))
+
+    word = vectorizer.get_feature_names() #所有文本的关键字
+    weight = tfidf.toarray()              #对应的tfidf矩阵
+
+    sFilePath = './tfidffile'
+    if not os.path.exists(sFilePath) :
+        os.mkdir(sFilePath)
+
+    # 这里将每份文档词语的TF-IDF写入tfidffile文件夹中保存
+    for i in range(len(weight)) :
+        print u"--------Writing all the tf-idf in the",i,u" file into ",sFilePath+'/'+string.zfill(i,5)+'.txt',"--------"
+        f = open(sFilePath+'/'+string.zfill(i,5)+'.txt','w+')
+        for j in range(len(word)) :
+            f.write(word[j]+"    "+str(weight[i][j])+"\n")
+        f.close()
 
 
 if __name__ == "__main__" :
