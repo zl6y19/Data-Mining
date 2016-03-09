4093a9c5303e54e665c88369f58db7f5c46513ba
Xinyan Lv <lvxinyan1992@gmail.com>
2016-03-09 17:40:30 +0000

    Add Spelling check
    Add English spelling check and Ancient Greek Spelling check.
    The spelling check need to put before the lemmatization, otherwise it
    will lower all terms which make spelling check not work.

--- data.py
+++ data.py
@@ -17,7 +17,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 
 
-nltk.download("stopwords")
+
 # 遍历指定目录，显示目录下的所有文件名
 
 def getWords(i,folds_name):
@@ -34,14 +34,23 @@ def getWords(i,folds_name):
         soul = BeautifulSoup(wp, "html.parser")
         content_original = ''
         for i in soul.find_all('span', class_ = "ocr_cinfo"):
-            #Lemmatization
-            content_original = content_original + ',' + lemma(i.text)
+
+            content_original = content_original + ',' + i.text
         # remove all non-alpha characters and convert to lowercase
         content = preprocessWords(content_original)
+
+        content = removeshort(content)
         # remove stopwords
+        content = correctword(content)
         content = removestopword(content)
         # remove short words
+        content = lemmatization(content)
+
+
+
         content = removeshort(content)
+
+        content = lowerword(content)
 
         for word in content:
             out.write('\t%s' % word)
@@ -52,18 +61,14 @@ def getWords(i,folds_name):
 
         # correct wrong spelling problem , act not well
         # try to use TF-IDF to do it
-        #content = correctword(content)
+
         #print content
 
 def preprocessWords(words):
-
-
-    # Split words by all non-alpha characters
-    # 用了这个后出现了u''!!!!!!!!!!!!!!!!!!
+    # Split words by all non alphabet character
     words = re.compile(r'[^A-Z^a-z]+').split(words)
-
     # Convert to lowercase
-    return [word.lower() for word in words if word != '']
+    return [word for word in words if word != '']
 
 def lowerword(words):
     return [word.lower() for word in words if 1 == 1]
@@ -74,31 +79,27 @@ def removestopword(words):
     return [word for word in words if word not in stop]
 
 def removeshort(words):
-
     # remove short word
     return [word for word in words if len(word) > 3]
 
-def correctword(words):
-    d = enchant.Dict("en_US")
-    # check the word by the build-in Dic
-    rightwords = ''
+def lemmatization(words):
+    #lemmatization
+    return [lemma(word) for word in words if word != '']
 
-    for word in words:
-        print d.check(word)
-        if d.check(word) == True:
-            rightwords = rightwords + ' ' + word
-            print word
-        else:
-            print word
-            #suggestion = d.suggest(word)
-            #print suggestion
-            #rightwords = rightwords + ' ' + suggestion[0]
-    return rightwords
+def correctword(words):
+    #d_us = enchant.Dict("en_US")
+    d_gb = enchant.Dict("en_GB")
+    d_g = enchant.Dict("grc_GR")
+    # check the word by the build-in Dic
+
+
+
+    return [word for word in words if d_g.check(word) == True or d_gb.check(word) == True]
     # if true : add to string else check the suggestion
 
 def Tfidf(filelist):
 
-    corpus = []  #存取100份文档的分词结果
+    corpus = []  #get the content from all files
     for ff in glob.glob(os.path.join(filelist, "*.txt")):
         f = open(ff,'r+')
         content = f.read()
@@ -107,14 +108,18 @@ def Tfidf(filelist):
 
     vectorizer = CountVectorizer()
     transformer = TfidfTransformer()
+    # Transform the terms into terms frequency matrix
     tfidf = transformer.fit_transform(vectorizer.fit_transform(corpus))
 
-    word = vectorizer.get_feature_names() #所有文本的关键字
+    word = vectorizer.get_feature_names() #get the key words from all files
     weight = tfidf.toarray()              #对应的tfidf矩阵
 
     sFilePath = '/Users/lxy/GitHub/Data-Mining/'
-    if not os.path.exists(sFilePath) :
+    if not os.path.exists(sFilePath):
         os.mkdir(sFilePath)
+
+    #print word
+    #print weight
 
     # 这里将每份文档词语的TF-IDF写入tfidffile文件夹中保存
     for i in range(len(weight)) :
@@ -131,13 +136,14 @@ if __name__ == "__main__" :
     file_path = r"/Users/lxy/Desktop/gap-html/"
     i = 0
     text_path = r"/Users/lxy/GitHub/Data-Mining/"
+    print enchant.list_languages()
 
-    #for folds_name in glob.glob(os.path.join(file_path, "*")):
-     #   i = i + 1
-      #  k = str(i)
-       # finalword = getWords(i,folds_name)
-        #if finalword == '1':
-         #   print "finish write fold" + k
+    for folds_name in glob.glob(os.path.join(file_path, "*")):
+        i = i + 1
+        k = str(i)
+        finalword = getWords(i,folds_name)
+        if finalword == '1':
+            print "finish write fold" + k
 
     final_TF = Tfidf(text_path)
     if final_TF == '1':
