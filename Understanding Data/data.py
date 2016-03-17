35003448db935f725087aac5cbb51c16b3be3e9a
Xinyan Lv <lvxinyan1992@gmail.com>
2016-03-17 16:10:36 +0000

    Remove test and encapsulation

--- Understanding Data/data.py
+++ Understanding Data/data.py
@@ -20,6 +20,7 @@ from sklearn.decomposition import PCA
 from sklearn.decomposition import NMF
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.cluster import KMeans
+from sklearn.manifold import MDS
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
@@ -29,7 +30,12 @@ import csv
 import scipy.cluster.hierarchy as hcluster
 from sklearn.externals import joblib
 import pandas as pd
-
+from scipy.cluster.hierarchy import ward, dendrogram, linkage
+from scipy.cluster.hierarchy import cophenet
+from scipy.spatial.distance import pdist
+import seaborn as sns
+from sklearn import manifold
+from matplotlib.collections import LineCollection
 
 #nltk.download("stopwords")
 
@@ -125,40 +131,26 @@ def Tfidf(filelist):
         #content = lowerword(content)
         corpus.append(content)
 
-    #vectorizer = CountVectorizer()
-    #transformer = TfidfTransformer()
-    TfidfVectorizer1 = TfidfVectorizer(max_df = 0.75, min_df = 0.2, stop_words='english')
-    # CountVectorizer().fit_transform: Transform the terms into terms frequency matrix
-    #freq_matrix = vectorizer.fit_transform(corpus)
-    #tfidf = transformer.fit_transform(freq_matrix)
+    # Set the parameters of TF-IDF
+    TfidfVectorizer1 = TfidfVectorizer(max_df = 0.65, min_df = 0.4, stop_words='english')
 
+    # Transform the terms into terms frequency matrix
     tfidf = TfidfVectorizer1.fit_transform(corpus)
 
 
-
-    #word = vectorizer.get_feature_names() #get the key words from all files
     word = TfidfVectorizer1.get_feature_names() #get the key words from all files
     print ('Features length: ' + str(len(word)))
-    weights = tfidf.toarray()              #对应的tfidf矩阵
+    weights = tfidf.toarray()              # get TF-IDF matrix
 
 
-
-    '''sFilePath = '/Users/lxy/Desktop/'
-    if not os.path.exists(sFilePath):
-        os.mkdir(sFilePath)
-
-   # 这里将每份文档词语的TF-IDF写入tfidffile文件夹中保存
-    for i in range(len(weight)):
-        print u"--------Writing all the tf-idf in the",i,u" file into ",sFilePath+'/'+string.zfill(i,5)+'.txt',"--------"
-        f = open(sFilePath+'/'+string.zfill(i,5)+'.txt','w+')
-        #csvfile = file('csv_test.csv', 'wb')
-        #writer = csv.writer(csvfile)
-        #writer.writerow(['text1','text2','text3','text4','text5','text6','text7','text8','text9','text10','text11','text12','text13','text14','text15','text16','text17','text18','text19','text20','text21','text22','text23','text24'])
-        for j in range(len(word)):
-            f.write(word[j]+"    "+str(weight[i][j])+"\n")
-            #writer.writerow(word[j] +','+ str(weight[i][j]))
-        f.close()
-        #csvfile.close()'''
+    # produce the warm-cold diagram for the similarity between texts
+    '''norm = np.linalg.norm(weights,axis=1).reshape((weights.shape[0],1))
+    weights2 = np.dot(weights, weights.T) / np.dot(norm, norm.T)
+    sns.heatmap(weights2, annot=True, center=0, cmap='coolwarm') # RdBu_r coolwarm
+    plt.title('Similarity by TF-IDF', fontsize=60)
+    plt.savefig('similarity.png', dpi=500, figsize=(2000, 2000))
+    plt.show()
+    plt.close()'''
 
 
 
@@ -168,22 +160,20 @@ def Tfidf(filelist):
 
 def k_means(weights, word):
 
-    #PCA
-    #pca = PCA(n_components=200)             #输出两维
-    #newData = pca.fit_transform(weights)   #载入N维
+
     print ('Start Kmeans:')
-    true_k = 3
-    clf = KMeans(n_clusters=true_k, max_iter=500)   #need to find a good way to set the K
+    true_k = 5
+    # Set the parameter of K-means
+    clf = KMeans(n_clusters=true_k, max_iter=500, n_init=50)   #need to find a good way to set the K
     s = clf.fit(weights)
-    #s = clf.fit(newData)
     print (s)
 
 
-    #中心点
+    # print centroid points
     print(clf.cluster_centers_)
 
-    #每个样本所属的簇
-    label = []               #存储1000个类标 4个类
+    # Print clusters for each samples
+    label = []
     print(clf.labels_)
     i = 1
     while i <= len(clf.labels_):
@@ -191,27 +181,30 @@ def k_means(weights, word):
         label.append(clf.labels_[i-1])
         i = i + 1
 
-    #用来评估簇的个数是否合适，距离越小说明簇分的越好，选取临界点的簇个数
+    # evaluate the number of clusters
     print(clf.inertia_)
 
 
-
+    # Print top terms
     print("Top terms:")
     order_centroids = clf.cluster_centers_.argsort()[:, ::-1]
-    #terms = vectorizer.get_feature_names()
     for i in range(true_k):
         print ("Cluster %d:" % i,)
         for ind in order_centroids[i, :10]:
             print (' %s' % word[ind],)
+            print (weight[i][ind])
     print()
 
 
     #PCA
-    pca = PCA(n_components=3)             #输出两维
-    newData = pca.fit_transform(weights)   #载入N维
-    print (newData)
+    #pca = PCA(n_components=3)             # Set the output dimension
+    #newData = pca.fit_transform(weights)   # Put the data in
+    #print (newData)
+    #MDS
+    mds = manifold.MDS(n_components=2, dissimilarity='euclidean')
+    newData = mds.fit_transform(weights)
 
-    #5A景区
+    #visualisation
     x1 = []
     y1 = []
     #z1 = []
@@ -224,7 +217,7 @@ def k_means(weights, word):
 
         i = i + 1
 
-    #动物
+
     x2 = []
     y2 = []
     #z2 = []
@@ -237,7 +230,7 @@ def k_means(weights, word):
 
         i = i + 1
 
-    #人物
+
     x3 = []
     y3 = []
     #z3 = []
@@ -250,7 +243,7 @@ def k_means(weights, word):
 
         i = i + 1
 
-    #国家
+
     x4 = []
     y4 = []
     #z4 = []
@@ -275,21 +268,69 @@ def k_means(weights, word):
 
         i = i + 1
 
-    #四种颜色 红 绿 蓝 黑
+    # produce the diagram
+    plt.title('K-means Clustering with PCA', fontsize=20)
+    plt.xlabel('Dimension 1', fontsize=15)
+    plt.ylabel('Dimension 2', fontsize=15)
+
     plt.plot(x1, y1, 'or')
     plt.plot(x2, y2, 'og')
     plt.plot(x3, y3, 'ob')
     plt.plot(x4, y4, 'ok')
     plt.plot(x5, y5, 'oy')
+    #plt.savefig('k-means.png', dpi=500)
     plt.show()
+    plt.close()
 
     return 1
 
 
-'''def hiercluster(weight):
-    pca = PCA(n_components=2)             #输出两维
-    newData = pca.fit_transform(weight)   #载入N维
-    print newData '''
+def hiercluster(weight, word):
+    linkage_matrix = ward(weight) #define the linkage_matrix using ward clustering pre-computed distances
+    fig, ax = plt.subplots(figsize=(15, 10)) # set size
+    ax = dendrogram(linkage_matrix, orientation="top", leaf_font_size=15.);
+
+
+
+    plt.title('Hierarchical Clustering' ,fontsize=30)
+    plt.xlabel('Text index ', fontsize=20)
+    plt.ylabel('Distance', fontsize=20)
+    plt.tick_params(\
+        axis= 'x',          # changes apply to the x-axis
+        which='both',      # both major and minor ticks are affected
+        bottom='on',      # ticks along the bottom edge are on
+        top='on',         # ticks along the top edge are on
+        labelbottom='on')
+
+    plt.tight_layout() #show plot with tight layout
+
+
+    plt.savefig('ward_clusters.png', dpi=400) #save figure as ward_clusters
+    plt.show()
+    plt.close()
+
+# Function for testing the MDS results
+def MDS(weight):
+    clf = manifold.MDS(n_components=2, metric=True, n_init=4, max_iter=300, verbose=0, eps=0.001, n_jobs=1, random_state=None, dissimilarity='euclidean')
+    pos = clf.fit_transform(weight)
+    #clf = PCA(n_components=2)
+    #mdsf = clf.fit_transform(mdsf)
+
+
+    # produce the diagram
+    plt.title('MDS', fontsize=20)
+    plt.xlabel('Dimension 1', fontsize=15)
+    plt.ylabel('Dimension 2', fontsize=15)
+
+    plt.scatter(pos[:, 0], pos[:, 1], s=20, c='g')
+    plt.savefig('mds.png', dpi=500)
+    plt.show()
+    plt.close()
+
+
+
+    return 1
+
 
 
 if __name__ == "__main__" :
@@ -311,7 +352,9 @@ if __name__ == "__main__" :
             print "finish write fold" + k'''
     #ready = repreprocess(text_path_personal)
     weight, word= Tfidf(text_path)
+    #test3 = MDS(weight)
     test = k_means(weight, word)
+    test2 = hiercluster(weight, word)
     print ("Finish")
 
 
